e91a15f4cbbb5bbe4d19a36a51652f5118dc1c75
JDOG <ortizg.jdavid@javeriana.edu.co>
2026-09-20 20:45:28 -0500

    [Fix]: Style

--- src/main/java/com/typeerror/myt/errors/InformacionIncompletaException.java
+++ src/main/java/com/typeerror/myt/errors/InformacionIncompletaException.java
@@ -1,6 +1,6 @@
 package com.typeerror.myt.errors;
 
-public class InformacionIncompletaException extends RuntimeException {
+public class InformacionIncompletaException extends IllegalArgumentException {
     public InformacionIncompletaException() {
         super("Se ha intentado completar una operación sin rellenar un campo obligatorio y no puede estar vacío.");
     }
--- src/main/java/com/typeerror/myt/errors/InformacionNoValidaException.java
+++ src/main/java/com/typeerror/myt/errors/InformacionNoValidaException.java
@@ -1,6 +1,6 @@
 package com.typeerror.myt.errors;
 
-public class InformacionNoValidaException extends RuntimeException {
+public class InformacionNoValidaException extends IllegalArgumentException {
     public InformacionNoValidaException() {
         super("La información proporcionada no es válida.");
     }
